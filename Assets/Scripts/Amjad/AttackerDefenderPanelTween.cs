7c4c0045d75142ccbc19cccd93f2e6de6e9a8bca
Amjad Mohamed <amjadmohamed55@gmail.com>
2023-06-12 10:52:19 +0300

    Tween Bug Fixes

--- Assets/Scripts/Amjad/AttackerDefenderPanelTween.cs
+++ Assets/Scripts/Amjad/AttackerDefenderPanelTween.cs
@@ -26,6 +26,7 @@ public class AttackerDefenderPanelTween : MonoBehaviour
             {
                 _panelTweener?.Kill(); // Kill any previous tweens
                 _panel.SetActive(true);
+                //_panel.transform.localScale = new Vector3(0, -2000, 0);
                 _panelTweener = _panel.transform.DOLocalMove(new Vector3(0, 70, 0), 1f);
             }
             else if (Input.GetMouseButtonUp(0) && _panel.activeSelf)
