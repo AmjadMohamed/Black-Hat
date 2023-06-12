0dbe838ad9ed43f59a034a3254852f6b3f5410a5
Amjad Mohamed <amjadmohamed55@gmail.com>
2023-06-12 14:46:49 +0300

    Added New Adware Walk Anim And Made Ads Depend On Adware Count

--- Assets/Scripts/Yasser/Abilities/AdwareAbility.cs
+++ Assets/Scripts/Yasser/Abilities/AdwareAbility.cs
@@ -3,6 +3,30 @@ using UnityEngine;
 
 public class AdwareAbility : GenericAbility, IAbility
 {
+    void Update()
+    {
+        base.Update();
+        if (MalwaresManager.Instance.AdwareCount >= numberOfMalwareNeededToUse)
+        {
+            if (EnergyManager.Instance._energy < cost || _nextUseTime > Time.time)
+            {
+                _card.interactable = false;
+                cooldownText.gameObject.SetActive(true);
+                _isOnCooldown = true;
+            }
+            else
+            {
+                _card.interactable = true;
+                cooldownText.gameObject.SetActive(false);
+                _isOnCooldown = false;
+            }
+        }
+        else
+        {
+            _card.interactable = false;
+        }
+    }
+
 
     public void Use()
     {
